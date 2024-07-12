d75832062840b6165e80f4047bdca6fcde2fc5cf
ValeriaKornienko <valebog94@gmail.com>
2024-07-12 20:58:56 +0200

    Lesson10

--- src/homeworks/Homework09/Homework09.tsx
+++ src/homeworks/Homework09/Homework09.tsx
@@ -1,4 +1,5 @@
 import { useState, ChangeEvent, useEffect } from "react";
+import axios, { Axios, AxiosError } from "axios";
 
 import Button from "components/Button/Button";
 
@@ -7,24 +8,43 @@ import { Homewokr09Wrapper, JokeCard, Joke } from "./styles";
 function Homework09() {
   const [randomJoke, setRandomJoke] = useState<string | undefined>(undefined);
   const [error, setError] = useState<string | undefined>(undefined);
+  {
+    /* const getRandomJoke = async () => {
+      setError(undefined);
+      setRandomJoke(undefined);
+      
+      const response = await fetch(
+        "https://official-joke-api.appspot.com/random_joke"
+      );
+      const result = await response.json();
+      console.log(result)
+  
+      if (response.ok) {
+  
+        setRandomJoke(`${result.setup} ${result.punchline}`);
+      } else {
+        setError("Ошибка при получении данных");
+      }
+    };
+  
+    useEffect(() => {
+      getRandomJoke();
+    }, []);
+  */
+  }
   const getRandomJoke = async () => {
     setError(undefined);
     setRandomJoke(undefined);
-    
-    const response = await fetch(
-      "https://official-joke-api.appspot.com/random_joke"
-    );
-    const result = await response.json();
-    console.log(result)
-
-    if (response.ok) {
-
-      setRandomJoke(`${result.setup} ${result.punchline}`);
-    } else {
-      setError("Ошибка при получении данных");
+    try {
+      const response = await axios.get(
+        "https://official-joke-api.appspot.com/random_joke"
+      );
+      setRandomJoke(`${response.data.setup} ${response.data.punchline}`);
+    } catch (error: any) {
+      setError(error.message);
+    } finally {
     }
   };
-
   useEffect(() => {
     getRandomJoke();
   }, []);
@@ -35,7 +55,11 @@ function Homework09() {
         {randomJoke && <Joke>{randomJoke}</Joke>}
         {error && <Joke>{error}</Joke>}
       </JokeCard>
-      <Button type="submit" name="Get new joke" onClick={getRandomJoke}></Button>
+      <Button
+        type="submit"
+        name="Get new joke"
+        onClick={getRandomJoke}
+      ></Button>
     </Homewokr09Wrapper>
   );
 }
